fe1c031a8231ab6ebf753197878d3980f9b95ed0
Durjoy Dey <durjoydey001@gmail.com>
2020-12-16 00:55:55 +0600

    :boom:

---
+++ Refused Bequest/Code solving the problem of Refused Bequest.java
@@ -0,0 +1,10 @@
+    public class Vehicle  
+    {  
+    }   
+    public class Car extends Vehicle  
+    { 
+       void Drive() {....}  
+    }  
+    public class Plane extends Vehicle  
+    {  
+    }  
